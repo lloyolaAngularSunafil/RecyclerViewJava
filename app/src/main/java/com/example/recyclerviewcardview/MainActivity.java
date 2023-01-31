2eeaf347b8c2a2c677e776feb0a4969283adde88
Luis Nazario Loyola Aquino <lloyola@sunafil.gob.pe>
2023-01-31 12:57:06 -0500

    recyclerView Java

---
+++ app/src/main/java/com/example/recyclerviewcardview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.recyclerviewcardview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    List<ListElement> listElementList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+    public void init(){
+        listElementList = new ArrayList<>();
+        listElementList.add(new ListElement("#775447","Pedro","México","Activo"));
+        listElementList.add(new ListElement("#607d8b","Julio","Tabasco","Activo"));
+        listElementList.add(new ListElement("#03a9f4","Alejandra","ChiHuahua","Cancelado"));
+        listElementList.add(new ListElement("#f44336","Jessica","Durando","Inactivo"));
+        listElementList.add(new ListElement("#009688","Armando","Yucatán","Inactivo"));
+
+        ListAdapter listAdapter = new ListAdapter(listElementList,this);
+        RecyclerView recyclerView = findViewById(R.id.listRecyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+
+    }
+}
